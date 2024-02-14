d11154c5e377eceb730bd51b574ac998988152e9
Kavinkumars23 <141397995+Kavinkumars23@users.noreply.github.com>
2024-02-15 00:34:11 +0530

    Updated backend

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
             <div className='max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full px-4 md:flex-row'>
                 <div className='flex flex-col justify-center h-full'>
-                    <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full stack Developer</h2>
+                    <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Backend Developer</h2>
                     <p className='text-gray-500 py-4 max-w-lg'>
                         I have a year of experience in building and designing web applications. I love to work on web applications using technologies like React, tailwind, ASP .Net and MSSQl.
                     </p>
