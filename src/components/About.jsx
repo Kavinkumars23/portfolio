f9b6a40eac294d214a63dfae3ae469aff0184fe9
Kavinkumars23 <141397995+Kavinkumars23@users.noreply.github.com>
2024-02-15 01:13:46 +0530

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,33 +3,36 @@ import React from 'react'
 const About = () => {
     return (
         <div name="about"
-        className='w-full h-screen bg-gradient-to-b from-gray-800 via-black to-gray-800 text-white sm:mt-3'>
+            className='w-full h-screen bg-gradient-to-b from-gray-800 via-black to-gray-800 text-white'>
             <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
                         About
-                        </p>
-                        </div>
-                    <p className='text-xl mt-10'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quisquam fugiat 
-                        id corrupti quidem suscipit consectetur
-                         vel porro corporis ducimus. Eaque eveniet libero architecto harum expedita, corrupti iste voluptates
-                          debitis nam eius est error ipsum facere repellendus odio illo ratione nisi! Veritatis sunt nisi ipsum commodi
-                           soluta nobis ex itaque.
-                    </p>
-
-                    <br />
-
-                    <p className='text-xl'>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex velit ea necessitatibus
-                         ipsa numquam possimus earum commodi veritatis id, fugiat et voluptas consequuntur,
-                          aliquid aspernatur dicta cum quasi a delectus impedit? Dolore ab ullam consequuntur eveniet est explicabo
-                         obcaecati facilis, necessitatibus corporis doloribus ducimus, rem voluptatem fugit eaque! Quod, fugit.
                     </p>
                 </div>
+                <p className='text-xl mt-10'>
+                    I am a budding software developer with a robust understanding of programming
+                    languages such as React, JavaScript, HTML, CSS, Tailwind CSS, ASP.NET MVC, and MSSQL.
+                    My proficiency extends beyond coding; I bring strong analytical skills and a fervent passion for problem-solving.
+                    Eager to learn and grow, I thrive in dynamic environments, consistently delivering effective solutions.
+                    My commitment to continuous learning fuels my adaptability, making me adept at embracing new technologies. 
+                    With a focus on clean and efficient coding practices, I am poised to contribute innovative ideas and collaborate
+                    seamlessly within a team. Ready to embark on a rewarding journey, I am excited about the prospect of
+                    creating impactful and scalable software solutions.
+                </p>
+
+                <br />
+
+                <p className='text-xl'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex velit ea necessitatibus
+                    ipsa numquam possimus earum commodi veritatis id, fugiat et voluptas consequuntur,
+                    aliquid aspernatur dicta cum quasi a delectus impedit? Dolore ab ullam consequuntur eveniet est explicabo
+                    obcaecati facilis, necessitatibus corporis doloribus ducimus, rem voluptatem fugit eaque! Quod, fugit.
+                </p>
             </div>
-            
-     
+        </div>
+
+
     )
 }
 
